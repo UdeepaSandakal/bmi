cb77465a32877b4b09005659dc0e2d877ade4817
UdeepaSandakal <2017asp02@vau.jfn.ac.lk>
2023-03-13 19:53:53 +0530

    Creating 3rd element -slider bar

--- lib/bmi_veiw.dart
+++ lib/bmi_veiw.dart
@@ -81,6 +81,35 @@ class _BMIViewState extends State<BMIView> {
               ],
             ),
           ),
+          Container(
+            height: 200,
+            width: double.infinity,
+            color: Colors.black38,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'Height',
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                ),
+                Text(
+                  '183cm', //This is just hard coded now
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                Slider(
+                  value: 150,
+                  onChanged: (value) {},
+                  min: 0,
+                  max: 200,
+                  activeColor: Colors.amber,
+                  thumbColor: Colors.pinkAccent,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
